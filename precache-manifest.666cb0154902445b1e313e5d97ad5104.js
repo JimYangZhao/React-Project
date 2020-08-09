c0410f82828957f33ee925f11ea9d56b18096ec2
JimYangZhao <yangzhao264@gmail.com>
2020-08-09 02:46:09 -0230

    Updates

---
+++ precache-manifest.666cb0154902445b1e313e5d97ad5104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9026b1ba5c29ab852cff4865a72e8b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f934afe11b07459cc750",
+    "url": "./static/css/main.78833b75.chunk.css"
+  },
+  {
+    "revision": "67c08aa6ffd114590d97",
+    "url": "./static/js/2.f7a5d80e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f7a5d80e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f934afe11b07459cc750",
+    "url": "./static/js/main.89d7fa72.chunk.js"
+  },
+  {
+    "revision": "babf34618f3c8cd08e39",
+    "url": "./static/js/runtime-main.b4f3ddbb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Project/Hangman-React-Project/public/precache-manifest.ab77397ebdcbe4f7b3bf25df8c575c61.js"
+  "./precache-manifest.666cb0154902445b1e313e5d97ad5104.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Project/Hangman-React-Project/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
