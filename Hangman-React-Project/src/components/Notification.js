d82e53030ee207b9da1d26d1157abf7b406c9898
Yang <yangzhao264@gmail.com>
2020-08-09 10:22:52 -0230

    Update Notification.js

--- Hangman-React-Project/src/components/Notification.js
+++ Hangman-React-Project/src/components/Notification.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Notification = ({showNotification}) => {
     return (
-        <div className={`notification-container' ${showNotification ? 'show' : ''}`}>
+        <div className={`notification-container ${showNotification ? 'show' : ''}`}>
             <p>You have already entered this letter</p>
         </div>
     )
