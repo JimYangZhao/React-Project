e86e33a3e4e2a594f6ad1b206d6d8bd79434c736
JimYangZhao <yangzhao264@gmail.com>
2020-08-09 02:40:35 -0230

    Updates

---
+++ precache-manifest.ab77397ebdcbe4f7b3bf25df8c575c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820a6eb74ee2ecaac5f00a35590df90c",
+    "url": "/React-Project/Hangman-React-Project/public/index.html"
+  },
+  {
+    "revision": "f934afe11b07459cc750",
+    "url": "/React-Project/Hangman-React-Project/public/static/css/main.78833b75.chunk.css"
+  },
+  {
+    "revision": "67c08aa6ffd114590d97",
+    "url": "/React-Project/Hangman-React-Project/public/static/js/2.f7a5d80e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Project/Hangman-React-Project/public/static/js/2.f7a5d80e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f934afe11b07459cc750",
+    "url": "/React-Project/Hangman-React-Project/public/static/js/main.89d7fa72.chunk.js"
+  },
+  {
+    "revision": "1a4f79d4c235570d7234",
+    "url": "/React-Project/Hangman-React-Project/public/static/js/runtime-main.ea82857d.js"
+  }
+]);
